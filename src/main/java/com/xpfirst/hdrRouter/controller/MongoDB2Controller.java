7cd1f6667e813ff7e42f3348c1101546e98954bf
nanjizhiyin <nanjizhiyin@gmail.com>
2017-12-19 16:22:43 +0800

    添加了总结

---
+++ src/main/java/com/xpfirst/hdrRouter/controller/MongoDB2Controller.java
@@ -0,0 +1,123 @@
+package com.xpfirst.hdrRouter.controller;
+
+
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
+import com.mongodb.BasicDBObject;
+import com.mongodb.MongoClient;
+import com.mongodb.MongoClientURI;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoCursor;
+import com.mongodb.client.MongoDatabase;
+import org.bson.Document;
+import org.bson.conversions.Bson;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+
+
+
+/**
+ * Copyright (C) 北京学信科技有限公司
+ *
+ * @Des:
+ * @Author: Gaojindan
+ * @Create: 2017/12/14 下午4:53
+ **/
+
+@Controller
+@RequestMapping(value = "mongodb2")
+public class MongoDB2Controller {
+
+    private MongoClientURI connectionString = new MongoClientURI("mongodb://gaojindan:gaojindan001@121.40.180.70:27017/gaojindandb");
+    private MongoClient mongoClient = new MongoClient(connectionString);
+    private MongoDatabase database = mongoClient.getDatabase("gaojindandb");
+    private MongoCollection<Document> paperColl = database.getCollection("xsl_paper");
+    private MongoCollection<Document> studentColl = database.getCollection("xsl_student");
+    private MongoCollection<Document> recordsColl = database.getCollection("xsl_records");
+//    private MongoCollection<Document> scoreColl = database.getCollection("xsl_score");
+
+
+    String[] students = {"100001","100002","100003","100004"};
+
+    String[] studentNames = {"郜1","郜2","郜3","郜4"};
+
+    //        添加4个试题的正确答案
+    String[] answers = {"a","b","c","d","e"};
+    //    试卷ID
+    private int paperID = 1;
+
+    @RequestMapping(value = "/")
+    public ModelAndView index(ModelMap modelMap){
+        modelMap.put("title","mongoDB操作");
+        return new ModelAndView("mongodb/index");
+    }
+    /**
+     * @Author: Gaojindan
+     * @Create: 2017/12/12 下午2:20
+     * @Des: 试卷操作类,添加4条记录
+     * @Param:
+     * @Return:
+     */
+    @RequestMapping(value = "paper")
+    public ModelAndView paper(ModelMap modelMap){
+        modelMap.put("title","试卷操作");
+
+        //设置为无效
+//        paperColl.updateMany(eq("paperID", paperID), new Document("$set", new Document("isEffective", false)));
+        paperColl.deleteMany(new Document());
+
+
+        List<Document> docList = new ArrayList<Document>();
+        for (int j = 0; j < answers.length; j++) {
+            docList.add(new Document("paperID", paperID)
+                    .append("paperNum",j+1)
+                    .append("paperAnswer",answers[j]));
+        }
+        paperColl.insertMany(docList);
+
+        modelMap.put("text","添加了4条试卷记录");
+        return new ModelAndView("mongodb/hello");
+    }
+    /**
+     * @Author: Gaojindan
+     * @Create: 2017/12/13 上午10:01
+     * @Des: 试卷信息
+     * @Param:
+     * @Return:
+     */
+    @RequestMapping(value = "paperinfo")
+    public ModelAndView paperinfo(ModelMap modelMap){
+        modelMap.put("title","试卷信息");
+
+        BasicDBObject doc5 = new BasicDBObject();
+        doc5.put("paperID", paperID);
+
+
+        //读取试卷信息
+        List<Map> infoList = new ArrayList<>();
+        MongoCursor<Document> cursor = paperColl.find(doc5).iterator();
+        try {
+            while (cursor.hasNext()) {
+                String tmpStr = cursor.next().toJson();
+                System.out.println(tmpStr);
+                JSONObject jObject = JSON.parseObject(tmpStr);
+                infoList.add(jObject);
+            }
+        } finally {
+            cursor.close();
+        }
+        modelMap.put("infoList",infoList);
+
+        return new ModelAndView("mongodb/info");
+    }
+
+
+}
--- src/test/java/com/xpfirst/hdrRouter/Weiming/Daka.java
+++ src/test/java/com/xpfirst/hdrRouter/Weiming/Daka.java
@@ -1,11 +1,9 @@
-package com.xpfirst.hdrRouter;
+package com.xpfirst.hdrRouter.Weiming;
 
-import net.sf.json.JSONObject;
 import org.apache.http.NameValuePair;
 import org.apache.http.client.CookieStore;
 import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.CloseableHttpResponse;
-import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.impl.client.BasicCookieStore;
 import org.apache.http.impl.client.CloseableHttpClient;
@@ -16,21 +14,18 @@ import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.IOException;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 /**
  * Copyright (C) 北京学信科技有限公司
  *
  * @Des:
  * @Author: Gaojindan
- * @Create: 2017/8/23 上午10:14
+ * @Create: 2017/12/19 下午4:13
  **/
-public class TEST {
-    private static final Logger log = LoggerFactory.getLogger(TEST.class);
+public class Daka {
+    private static final Logger log = LoggerFactory.getLogger(Daka.class);
 
     @Test
     //发送验证码,接口有bug,获取验证码,并不需要使用
@@ -48,7 +43,6 @@ public class TEST {
 
             params.add(new BasicNameValuePair("__xml", "wE2wwRUELalbSkCYUf5Kt+WiVMEVh5sftj9nhLg4cxzSWo2yDhhM86SPZIQTyS7K5Y5EMjg0bNPwBzoocuq7nemTLsKgSYM+3Zux7udBQSX/6ziqf+jqq+rI+uMevQvEg6P7ifkTdFPdxaEiekmRxS33/2WVq5JTKBa+Dx2jDSlZpdBQ/Z+WzBbPcMyO15AtyR+xkK48ZEahSsXFrRODZny1+WKr8mW3zAs8TgTnV/wzf+BQhnrUBX9scVUIqLYe"));
             HttpPost httppost = new HttpPost(url);
-            httppost.setHeader("User-Agent", "Mozilla/5.0 (iPhone; CPU iPhone OS 11_0_2 like Mac OS X) AppleWebKit/604.1.38 (KHTML, like Gecko) Mobile/15A421 MicroMessenger/6.5.18 NetType/WIFI Language/zh_CN");
 
             httppost.setEntity(new UrlEncodedFormEntity(params, "utf-8"));
 
@@ -66,8 +60,4 @@ public class TEST {
         }
     }
 
-    @Test
-    public void makeMM(){
-
-    }
 }
